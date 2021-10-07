8ff61ccf4da8467efe0040dd03b8454972cebd60
Adline WILSON <wilson.adline@gmail.com>
2021-10-07 22:46:38 +0200

    page produit sans le title

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Articles;
 use App\Entity\Categories;
+use App\Repository\ArticlesRepository;
 use App\Repository\CategoriesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,4 +20,22 @@ class MainController extends AbstractController
             'categorieByTissu' => $categories->findBy(['typeCategories' =>2])
         ]);
     }
+
+    #[Route('/categorie/articles_mercerie/{id}', name: 'app_articles_mercerie')]
+    public function articles_mercerie($id): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Articles::class);
+        $article = $repository->findBy(['categories'=>$id]);
+
+        // $article = $this->getDoctrine()->getRepository(Categories::class)->findBy(''=>$articles);
+
+        return $this->render('main/categoriemercerie.html.twig', [
+            'article'=>$article,
+            // dd($article)
+
+           
+        ]);
+    }
+
+
 }
