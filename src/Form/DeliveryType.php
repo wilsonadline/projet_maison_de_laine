4bd21f273340c6d74940cc26f8b10b97594d999d
Adline WILSON <wilson.adline@gmail.com>
2022-02-12 23:04:56 +0100

    form delivery

---
+++ src/Form/DeliveryType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Delivry;
+// use Doctrine\DBAL\Types\FloatType;
+// use Symfony\Component\Form\Extension\Core\Type\Fl;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DeliveryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('options', TextType::class)
+            ->add('price', NumberType::class)
+            ->add('delai', NumberType::class )
+            ->add('imageFile', FileType::class)
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Delivry::class,
+        ]);
+    }
+}
