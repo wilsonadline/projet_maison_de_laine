33b57e18d685504c064f93b252dbb729ed267fb5
Adline WILSON <wilson.adline@gmail.com>
2022-02-26 23:50:33 +0100

    fonction user delete user unverified + secu CsrfToken

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\UsersRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,8 +14,21 @@ class AdminController extends AbstractController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
+        return $this->render('admin/index.html.twig');
     }
+    #[Route('users', name: 'users')]
+    public function users(UsersRepository $usersRepository, EntityManagerInterface $em, HttpFoundationRequest $request): Response
+    {
+        if($this->isCsrfTokenValid('delete'.$this->getUser()->getId(), $request->query->get('csrf'))) {
+            $user = $usersRepository->unVerified();
+            foreach($user as $value){
+                $em->remove($value);
+                $em->flush();
+            }
+        }else{
+            dd("ahhhh");
+        }
+        return $this->redirectToRoute('app_home');
+    }
+
 }
