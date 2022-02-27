41daf3cc410cf1b3f3653faa13daaa21432c2977
Adline WILSON <wilson.adline@gmail.com>
2022-02-27 20:08:56 +0100

    clean code userController finction list

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -6,7 +6,6 @@ use App\Entity\Users;
 use App\Form\UsersType;
 use App\Repository\UsersRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Knp\Component\Pager\PaginationInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,7 +23,7 @@ class UsersController extends AbstractController
     }
 
     #[Route('/users/list', name: 'users_list')]
-    public function users_list(UsersRepository $users, Request $request, PaginatorInterface $paginatorInterface ): Response
+    public function users_list(UsersRepository $users, Request $request): Response
     {
         return $this->render('admin/users/list.html.twig', [
             'users' => $users->findAll()
