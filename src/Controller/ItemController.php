262ecfef085a6eb1de0ef0dd18868da25c9c2511
Adline WILSON <wilson.adline@gmail.com>
2022-03-08 09:24:44 +0100

    clean code + comment code

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -9,9 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ItemController extends AbstractController
 {
-    #[Route('/categorie/articles_mercerie/item/{id}', name: 'app_items_mercerie')]
+    // fonction affichant l'article seulement avec son descriptif
+    #[Route('/item/{id}', name: 'app_items_mercerie')]
     public function articles_mercerie($id, ArticlesRepository $articlesRepository): Response
     {
+        // recup de l'article choisi grace à find by en parametre l'id
         return $this->render('main/items.html.twig', [
             'article'=>$articlesRepository->findBy(['id'=>$id])
         ]);
